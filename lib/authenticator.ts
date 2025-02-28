41c6b20a973b1677a350eae7fb3909790c9e6ef6
VikasMeshram2708 <meshramvikas744@gmail.com>
2025-02-28 12:04:18 +0530

    :sparkes: done.

--- lib/authenticator.ts
+++ lib/authenticator.ts
@@ -1,11 +1,22 @@
 export const authenticator = async () => {
   try {
-    const response = await fetch("http://localhost:3000/api/imagekit-auth");
+    const Production_BASE_URL = "https://dam-ruby.vercel.app";
+    const localBaseUrl = "http://localhost:3000";
+
+    let BASE_URL;
+
+    if (process.env.NODE_ENV === "production") {
+      BASE_URL = Production_BASE_URL;
+    } else {
+      BASE_URL = localBaseUrl;
+    }
+
+    const response = await fetch(`${BASE_URL}/api/imagekit-auth`);
 
     if (!response.ok) {
       const errorText = await response.text();
       throw new Error(
-        `Request failed with status ${response.status}: ${errorText}`,
+        `Request failed with status ${response.status}: ${errorText}`
       );
     }
 
